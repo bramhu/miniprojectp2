978a06245b8517d5c3d84a57ef583db2ebe1329f
bramhu <bram@MacBook-Pro-van-Bram-2.local>
2015-01-12 13:10:12 +0100

    Opdracht B12

---
+++ Opdracht_B12/GUI.java
@@ -0,0 +1,58 @@
+package opdracht_U_6;
+
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+public abstract class GUI implements ActionListener
+{
+	protected JFrame frame = new JFrame();
+	protected GridBagConstraints gbc = new GridBagConstraints();
+	private Font font = new Font("Arial", Font.BOLD, 20);
+	
+	public GUI(int width, int height, String title, boolean resizable)
+	{
+		frame.setLayout(new GridBagLayout());
+		gbc.insets = new Insets(5, 5, 5, 5);
+		gbc.anchor = GridBagConstraints.WEST;
+		
+		frame.setVisible(false);
+		frame.setTitle(title);
+		frame.setSize(width, height);
+		frame.setPreferredSize(new Dimension(width, height));
+		frame.setMinimumSize(new Dimension(width, height));
+		frame.setResizable(resizable);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+	}
+	
+	public GUI(int width, int height, String title)
+	{
+		this(width, height, title, false);
+	}
+	
+	public void setVisible(boolean visible)
+	{
+		frame.setVisible(visible);
+	}
+	
+	public boolean isVisible()
+	{
+		return frame.isVisible();
+	}
+	
+	public void add(JComponent comp)
+	{
+		add(comp, false);
+	}
+	
+	public void add(JComponent comp, boolean incr_y)
+	{
+		frame.add(comp, gbc);
+		if (incr_y)
+			gbc.gridy++;
+		comp.setFont(font);
+	}
+}
---
+++ Opdracht_B12/Main.java
@@ -0,0 +1,8 @@
+package opdracht_U_6;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new Lingo(500, 500, "Lingo");
+	}
+}
